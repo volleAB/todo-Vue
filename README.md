289a73f916468a008f1a6b8759ebc983579b21f2
volleAB <724958674@qq.com>
2018-04-07 14:43:07 +0800

    2018/04/07

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 
    **Practice makes perfect.**
 
-
-<img src="https://cn.vuejs.org/images/logo.png" width="200" height="200">
-<img src="./src/assets/images/webpack.png" width="200" height="200">
-<img src="https://git-scm.com/images/logo@2x.png" width="200" height="100">
-<img src="http://s.nodejs.cn/less/img/logo.png"  width="200" height="100">
+<div align=center>
+    <img src="https://cn.vuejs.org/images/logo.png" width="200" height="200">
+    <img src="./src/assets/images/webpack.png" width="200" height="200">
+    <img src="https://git-scm.com/images/logo@2x.png" width="200" height="100">
+    <img src="http://s.nodejs.cn/less/img/logo.png"  width="200" height="100">
+</div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'develoment';
+
+const config = {
+    target: 'web',
     entry: path.join(__dirname,'src/index.js'), //入口 输入
     //__dirname表示这个文件所在目录的地址-->根目录
     //path.join表示把第一个参数的路径和第二个参数的路径拼接起来，拼成绝对路径
@@ -44,5 +49,34 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    // process.env.NODE_ENV = develpment
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
 }
+
+if(isDev) {
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true,
+        },
+        hot: true
+        // historyFallback: {}
+        // open: true,  运行webpack时打开页面
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config;
